e54c2e52540742cd9a0cea13b4e5344a5ed19dd9
Geraldine Romero <geraldine.romero@softvision.com>
2019-09-24 17:59:00 -0300

    Creating a class in Java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myAge = 33 + 1;
+        System.out.println(myAge);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = mySecondNumber * 2;
+        int myTotal = myAge + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+    }
+}
