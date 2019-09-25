45e381bf148ffde038a8348140e3e7ffa172c070
Geraldine Romero <geraldine.romero@softvision.com>
2019-09-25 12:18:30 -0300

    Changes made in Java adding data types

--- Main.java
+++ Main.java
@@ -38,6 +38,43 @@ public class Main {
         byte myTestByteValue = (byte) (myMinByteValue /2);
         short myTestShortValue = (short) (myMinShortValue / 2);
         int myTestIntValue = (myMinIntValue / 2);
+
+        // number data types
+
+        float myFloatMinValue = Float.MIN_VALUE;
+        float myFloatMaxValue = Float.MAX_VALUE;
+        System.out.println("My float min value = " + myFloatMinValue);
+        System.out.println("My float max value = " + myFloatMaxValue);
+
+        double myDoubleMinValue = Double.MIN_VALUE;
+        double myDoubleMaxValue = Double.MAX_VALUE;
+        System.out.println("My double min value = " + myDoubleMinValue);
+        System.out.println("My double max value = " + myDoubleMaxValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.0f; // here we use "f" so it can be defined as float, or we have to cast
+        double myDoubleValue = 5.0;
+        System.out.println(myIntValue);
+        System.out.println(myFloatValue);
+        System.out.println(myDoubleValue);
+
+        //Converting a given number of pounds to kilograms
+
+        double onePoundInKg = 0.45359237;
+        double poundsToConvert = 150;
+        double resultKg = onePoundInKg * poundsToConvert;
+        System.out.println("Kg = " + resultKg);
+
+        // primitive types
+
+        char aChar = 'B';
+        char unicodeChar = '\u0392';
+        char charGreek = '\u03BB';
+        System.out.println(unicodeChar);
+        System.out.println(charGreek);
+
+        boolean myTrueBoolean = true; // declare with 1
+        boolean myFalseBoolean = false // declared with 0
     }
 
 }
